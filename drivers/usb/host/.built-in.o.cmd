61f3a5290122e2404dfe1c9a2b14588bb3d1200a
Yajnavalkya <yajnab@gmail.com>
2013-01-25 11:12:17 +0530

    Update

--- drivers/usb/host/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_drivers/usb/host/built-in.o :=  ../arm-2011.03/bin/arm-none-eabi-ld -EL    -r -o drivers/usb/host/built-in.o drivers/usb/host/ehci-hcd.o 
--- drivers/usb/storage/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_drivers/usb/storage/built-in.o :=  rm -f drivers/usb/storage/built-in.o; ../arm-2011.03/bin/arm-none-eabi-ar rcs drivers/usb/storage/built-in.o
