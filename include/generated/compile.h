ecf115d3af86248909fa7f1c3eb5646df9cd6bbe
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2014-06-24 12:42:34 +0530

    Cleanup

--- include/generated/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 3 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#3 PREEMPT Fri Jan 4 23:32:17 IST 2013"
-#define LINUX_COMPILE_TIME "23:32:18"
-#define LINUX_COMPILE_BY "yajnavalkya"
-#define LINUX_COMPILE_HOST "yajnavalkya-desktop"
-#define LINUX_COMPILE_DOMAIN "(none)"
-#define LINUX_COMPILER "gcc version 4.5.2 (Sourcery G++ Lite 2011.03-42) "
